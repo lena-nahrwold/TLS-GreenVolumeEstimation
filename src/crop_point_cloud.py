5fcf2a537cc2c399150cb19a6eef29b49642204a
lena <lena.nahrwold@posteo.de>
2026-04-16 18:04:35 +0200

    Add wkt extraction to crop_point_cloud.py

--- src/crop_point_cloud.py
+++ src/crop_point_cloud.py
@@ -3,15 +3,22 @@ import os
 import json
 import subprocess
 import numpy as np
-
+from osgeo import ogr
 
 def crop_point_cloud(pointcloud:str, shapefile:str, output_path:str, use_as_filter:bool) -> str:
+
+    ds = ogr.Open(shapefile)
+    layer = ds.GetLayer(0)
+    feat = layer.GetNextFeature()
+    geom = feat.GetGeometryRef()
+    wkt = geom.ExportToWkt()
+
     pipeline = {
         "pipeline": [
             pointcloud, 
             {
                 "type": "filters.crop",
-                "polygon": shapefile,
+                "polygon": wkt,
                 "outside": use_as_filter,
             },
             output_path
@@ -43,6 +50,8 @@ def main():
     use_as_filter = args.filter
 
     cropped_point_cloud = crop_point_cloud(pcd, shp, output, use_as_filter)
+
+    print(f"✓ Saved cropped point cloud to {cropped_point_cloud}.")
 
 
 if __name__ == '__main__':
